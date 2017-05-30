9d420742f58e798db06489efa36adf050117bec4
gcolaco <gustavo.colaco@hotmail.com>
2017-05-30 17:11:32 -0300

    entities bug fixed, UI created

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by Gustavo Colaco on 30/05/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
