69428bea34249035e460126140b6be255aef0a0e
gcolaco <gustavo.colaco@hotmail.com>
2017-05-31 16:10:39 -0300

    view controller update

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Gustavo Colaco on 31/05/17.
+//  Copyright © 2017 Gustavo Colaco. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+
+}
