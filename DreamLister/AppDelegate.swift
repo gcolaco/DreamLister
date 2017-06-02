0e5b347e7576d8ffac07987a61c863a61845f3a3
gcolaco <gustavo.colaco@hotmail.com>
2017-06-02 17:27:16 -0300

    fetchedResultsController

--- DreamLister/AppDelegate.swift
+++ DreamLister/AppDelegate.swift
@@ -91,3 +91,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+let ad = UIApplication.shared.delegate as! AppDelegate
+let context =  ad.persistentContainer.viewContext
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
